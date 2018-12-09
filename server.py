d16d63e90bc0050ef6322dabaddf4b40bc111f65
Laurence Ruberl <bknd225@gmail.com>
2018-12-09 16:31:49 -0500

    Add sending message back to sender

--- server.py
+++ server.py
@@ -5,50 +5,51 @@ from threading import Thread
 
 
 def accept_incoming_connections():
-    """Sets up handling for incoming clients."""
-    while True:
-        client, client_address = SERVER.accept()
-        print("%s:%s has connected." % client_address)
-        client.send(bytes("Greetings from the cave! Now type your name and press enter!", "utf8"))
-        addresses[client] = client_address
-        Thread(target=handle_client, args=(client,)).start()
+	"""Sets up handling for incoming clients."""
+	while True:
+		client, client_address = SERVER.accept()
+		print("%s:%s has connected." % client_address)
+		client.send(bytes("Greetings from the cave! Now type your name and press enter!", "utf8"))
+		addresses[client] = client_address
+		Thread(target=handle_client, args=(client,)).start()
 
 def handle_client(client):  # Takes client socket as argument.
-    """Handles a single client connection."""
+	"""Handles a single client connection."""
 
-    name = client.recv(BUFSIZ).decode("utf8")
-    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
-    client.send(bytes(welcome, "utf8"))
-    msg = "%s has joined the chat!" % name
-    broadcast(bytes(msg, "utf8"))
-    clients[client] = name
+	name = client.recv(BUFSIZ).decode("utf8")
+	welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
+	client.send(bytes(welcome, "utf8"))
+	msg = "%s has joined the chat!" % name
+	broadcast(bytes(msg, "utf8"))
+	clients[client] = name
 
-    while True:
-        msg = client.recv(BUFSIZ)
-        if msg != bytes("{quit}", "utf8"):
-            if msg.decode("utf8").startswith("@"):
-                receiver = msg.decode("utf8").split(" ")[0][1:]
-                msg = " ".join(msg.decode("utf8").split(" ")[1:])
-                private_msg(bytes(msg, "utf8"), receiver, name+": ")
-            else:
-                broadcast(msg, name+": ")
-        else:
-            client.send(bytes("{quit}", "utf8"))
-            client.close()
-            del clients[client]
-            broadcast(bytes("%s has left the chat." % name, "utf8"))
-            break
+	while True:
+		msg = client.recv(BUFSIZ)
+		if msg != bytes("{quit}", "utf8"):
+			if msg.decode("utf8").startswith("@"):
+				receiver = msg.decode("utf8").split(" ")[0][1:]
+				msg = " ".join(msg.decode("utf8").split(" ")[1:])
+				private_msg(bytes(msg, "utf8"), receiver, name+": ")
+				private_msg(bytes(msg, "utf8"), client, name+": ")
+			else:
+				broadcast(msg, name+": ")
+		else:
+			client.send(bytes("{quit}", "utf8"))
+			client.close()
+			del clients[client]
+			broadcast(bytes("%s has left the chat." % name, "utf8"))
+			break
 
 
 def broadcast(msg, prefix=""):  # prefix is for name identification.
-    """Broadcasts a message to all the clients."""
-    for sock in clients:
-        sock.send(bytes(prefix, "utf8")+msg)
+	"""Broadcasts a message to all the clients."""
+	for sock in clients:
+		sock.send(bytes(prefix, "utf8")+msg)
 
 def private_msg(msg, receiver, prefix=""):
-    for sock in clients:
-        if clients[sock] == receiver:
-            sock.send(bytes(prefix, "utf8")+msg)
+	for sock in clients:
+		if clients[sock] == receiver:
+			sock.send(bytes(prefix, "utf8")+msg)
 
 clients = {}
 addresses = {}
@@ -62,9 +63,9 @@ SERVER = socket(AF_INET, SOCK_STREAM)
 SERVER.bind(ADDR)
 
 if __name__ == "__main__":
-    SERVER.listen(5)
-    print("Waiting for connection...")
-    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
-    ACCEPT_THREAD.start()
-    ACCEPT_THREAD.join()
-    SERVER.close()
+	SERVER.listen(5)
+	print("Waiting for connection...")
+	ACCEPT_THREAD = Thread(target=accept_incoming_connections)
+	ACCEPT_THREAD.start()
+	ACCEPT_THREAD.join()
+	SERVER.close()
