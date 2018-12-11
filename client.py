f622d4f9fa6caf9cebc19730f13904cfff0db1f9
Sari S <sestissi16@earlham.edu>
2018-12-10 21:37:57 -0500

    added a user list listbox and frames to make it look nice

--- client.py
+++ client.py
@@ -70,7 +70,12 @@ if __name__ == "__main__":
 	top = tkinter.Tk()
 	top.title("Chatter")
 
-	messages_frame = tkinter.Frame(top)
+	groupUser_frame = tkinter.Frame(top)
+	groupUser_frame.pack(expand=True, fill=tkinter.BOTH)
+
+	messages_frame = tkinter.Frame(groupUser_frame)
+	groupMsgLabel = tkinter.Label(messages_frame, text="Group Chat")
+	groupMsgLabel.pack(expand=True, fill=tkinter.X)
 	my_msg = tkinter.StringVar()  # For the messages to be sent.
 	my_msg.set("Type your messages here.")
 	scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
@@ -79,21 +84,34 @@ if __name__ == "__main__":
 	scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 	msg_list.pack(side=tkinter.LEFT, expand=True, fill=tkinter.BOTH)
 	msg_list.pack()
-	messages_frame.pack(expand=True, fill=tkinter.BOTH)
+	messages_frame.pack(side=tkinter.LEFT, expand=True, fill=tkinter.BOTH)
 
-	pmSectionLabel = tkinter.Label(top, text="Private Messages:")
+	userlist_frame = tkinter.Frame(groupUser_frame)
+	userListLabel = tkinter.Label(userlist_frame, text="Available Users")
+	userListLabel.pack(expand=True, fill=tkinter.X)
+	scrollbar2 = tkinter.Scrollbar(userlist_frame)
+	user_list = tkinter.Listbox(userlist_frame, height=15, width=25, yscrollcommand=scrollbar2.set)
+	scrollbar2.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+	user_list.pack(side=tkinter.LEFT, expand=True, fill=tkinter.BOTH)
+	user_list.pack()
+	userlist_frame.pack(side=tkinter.RIGHT, expand=True, fill=tkinter.BOTH)
+
+
 	privateMsg_frame = tkinter.Frame(top)
+	pmSectionLabel = tkinter.Label(privateMsg_frame, text="Private Messages:")
 	pmSectionLabel.pack(expand=True, fill=tkinter.X)
 	privateMsg_frame.pack(expand=True, fill=tkinter.BOTH)
 
 	privLists = {}
 	privFrames = {}
 
-	entry_field = tkinter.Entry(top, textvariable=my_msg, width=45)
+	entry_frame = tkinter.Frame(top)
+	entry_field = tkinter.Entry(entry_frame, textvariable=my_msg, width=45)
 	entry_field.bind("<Return>", send)
 	entry_field.pack()
-	send_button = tkinter.Button(top, text="Send", command=send)
+	send_button = tkinter.Button(entry_frame, text="Send", command=send)
 	send_button.pack()
+	entry_frame.pack(side=tkinter.BOTTOM, expand=True, fill=tkinter.X)
 
 	top.protocol("WM_DELETE_WINDOW", on_closing)
 
